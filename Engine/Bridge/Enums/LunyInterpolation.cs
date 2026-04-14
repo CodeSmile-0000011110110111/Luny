6c1a12c8ab7284d742b47238c7787c5ce822624d
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2026-04-14 18:09:35 +0200

    combine lerp blocks

---
+++ Engine/Bridge/Enums/LunyInterpolation.cs
@@ -0,0 +1,12 @@
+﻿namespace Luny.Engine.Bridge
+{
+	public enum LunyInterpolation
+	{
+		Instant, // Teleport to target
+		ConstantSpeed, // Vector3.MoveTowards (Constant velocity)
+		Linear, // Vector3.Lerp (Constant percentage)
+		Spherical, // Vector3.Slerp (Arcing motion)
+		SmoothStep, // Lerp with Ease-In/Out
+		SmoothDamp, // Vector3.SmoothDamp (Physics-based follow)
+	}
+}
