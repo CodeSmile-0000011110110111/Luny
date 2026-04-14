389976e4074464cc7246c53813842a85edea01e3
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2026-04-14 18:48:35 +0200

    add lookat lerp blocks

--- Engine/Bridge/Enums/LunyInterpolation.cs
+++ Engine/Bridge/Enums/LunyInterpolation.cs
@@ -6,6 +6,8 @@
 		ConstantSpeed, // Vector3.MoveTowards (Constant velocity)
 		Linear, // Vector3.Lerp (Constant percentage)
 		Spherical, // Vector3.Slerp (Arcing motion)
+		LinearUnclamped, // Vector3.Lerp (Constant percentage)
+		SphericalUnclamped, // Vector3.Slerp (Arcing motion)
 		SmoothStep, // Lerp with Ease-In/Out
 		SmoothDamp, // Vector3.SmoothDamp (Physics-based follow)
 	}
